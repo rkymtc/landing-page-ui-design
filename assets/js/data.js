06fe27be0bafad417f14fb1b3a1bb5f74efba968
unknown <rukiyemotcu@gmail.com>
2023-02-05 14:27:51 +0300

    send data to html

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,45 +2,48 @@
 const data = [
     {
         id: 1,
+        category: "web",
+        design: ("./assets/img/card-first.png"),
         profile: ("./assets/img/photo.png"),
         author: "Jenny Molla",
-        title: "Web Card Design",
+        title: "Web",
         viewTime: "lastest",
         tool: "sketch",
         toolImg: ("./assets/img/sketch.svg"),
         code: 25,
         appreciate: 25,
-        category: "web",
-        design: ("./assets/img/card-first.png")
     },
     {
-        id: 2,
+        id: 2, 
+        category: "web",
+        design: ("./assets/img/card-second.png"),
         profile: ("./assets/img/photo.png"),
         author: "Jenny Mollas",
-        title: "Web Card Design",
+        title: "Web",
         viewTime: "popular",
         tool: "sketch",
         toolImg: ("./assets/img/sketch.svg"),
         code: 25,
         appreciate: 25,
-        category: "web",
-        design: ("./assets/img/card-second.png")
     },
     {
         id: 3,
+        category: "web",
+        design: ("./assets/img/card-third.png"),
         profile: ("./assets/img/photo.png"),
         author: "Jenny Molla",
-        title: "Web Card Design",
+        title: "Web",
         viewTime: "lastest",
         tool: "sketch",
         toolImg: ("./assets/img/sketch.svg"),
         code: 12,
         appreciate: 12,
-        category: "mobile",
-        design: ("./assets/img/card-third.png")
+        
     },
     {
-        id: 4,
+        id: 4, 
+        category: "mobile",
+        design: ("./assets/img/card-fifth.png"),
         profile: ("./assets/img/photo-first.png"),
         author: "Lorenz",
         title: "Mobile App",
@@ -49,37 +52,40 @@ const data = [
         toolImg: ("./assets/img/figma.svg"),
         code: 24,
         appreciate: 24,
-        category: "mobile",
-        design: ("./assets/img/card-fifth.png")
+      
     },
     {
-        id: 5,
+        id: 5, 
+        category: "uı kit",
+        design: ("./assets/img/card-sixth.png"),
         profile: ("./assets/img/photo-first.png"),
         author: "Nick Mehta",
-        title: "Web site / 3D illustration",
+        title: "Web 3D illustration",
         viewTime: "lastest",
         tool: "adobe-ps",
         toolImg: ("./assets/img/adobe-ps.svg"),
         code: 77,
         appreciate: 69,
-        category: "uı kit",
-        design: ("./assets/img/card-sixth.png")
+       
     },
     {
         id: 6,
+        category: "uı kit",
+        design: ("./assets/img/card-seventh.png"),
         profile: ("./assets/img/photo-first.png"),
         author: "Test",
-        title: "Hero header concept",
+        title: "Web 3D illustration",
         viewTime: "popular",
         tool: "adobe-xd",
         toolImg: ("./assets/img/adobe-xd.svg"),
         code: 55,
         appreciate: 88,
-        category: "uı kit",
-        design: ("./assets/img/card-seventh.png")
+        
     },
     {
         id: 7,
+        category: "coded",
+        design: ("./assets/img/card-eighth.png"),
         profile: ("./assets/img/photo-second.png"),
         author: "Test",
         title: "Coded",
@@ -88,38 +94,38 @@ const data = [
         toolImg: ("./assets/img/studio.svg"),
         code: 72,
         appreciate: 114,
-        category: "coded",
-        design: ("./assets/img/card-eighth.png")
+        
     },
     {
         id: 8,
+        category: "coded",
+        design: ("./assets/img/card-ninth.png"),
         profile: ("./assets/img/photo-second.png"),
         author: "Saidur Rahman",
-        title: "Dashboard",
+        title: "Coded",
         viewTime: "popular",
         tool: "adobe-xd",
         toolImg: ("./assets/img/adobe-xd.svg"),
         code: 8,
         appreciate: 114,
-        category: "coded",
-        design: ("./assets/img/card-ninth.png")
+        
     },
     {
         id: 9,
+        category: "web",
+        design: ("./assets/img/card-fourth.png"),
         profile: ("./assets/img/photo-second.png"),
         author: "John Rocha",
-        title: "Web Site- Hero",
+        title: "Web",
         viewTime: "lastest",
         tool: "figma",
         toolImg: ("./assets/img/figma.svg"),
         code: 76,
         appreciate: 5,
-        category: "web",
-        design: ("./assets/img/card-fourth.png")
+        
     }
 ];
-
-const myİtems = document.querySelector('.design-card-wrapper');
+const myItems = document.querySelector('.design-card-wrapper');
 window.addEventListener("DOMContentLoaded", function () {
     const showInHtml = data.map((project) => {
         return`<div class="design-card col-md-6 col-lg-4  my-4">
@@ -145,6 +151,6 @@ window.addEventListener("DOMContentLoaded", function () {
         </div>
       </div>`
     })
-    myİtems.innerHTML = showInHtml.join(""); 
+    myItems.innerHTML = showInHtml.join(""); 
     // If you don’t stipulate how to perform the join (which is what toString is doing), it will default to a comma.
 });
